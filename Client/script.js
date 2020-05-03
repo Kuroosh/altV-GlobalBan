05915d160289c41978f0cfbecf7b004ab60236a3
Mario Franze <mfranze@codingden.de>
2020-05-03 19:07:38 +0200

    Changed from single to doubble quotes

--- Client/script.js
+++ Client/script.js
@@ -4,16 +4,16 @@
 ////////www.venox-reallife.com////////
 //----------------------------------//
 
-import alt from 'alt-client';
+import alt from "alt-client";
 import * as game from "natives";
 
 
-let localplayer = alt.Player.local;
+let localPlayer = alt.Player.local;
 let kicked = false;
 
 alt.log("VnXGlobalSystemsClient:Loaded");
 
-alt.onServer('VnXGlobalSystemsClient:GetDiscordID', () => {
+alt.onServer("VnXGlobalSystemsClient:GetDiscordID", () => {
     try {
         if (!alt.Discord.currentUser)
             return null;
@@ -24,16 +24,16 @@ alt.onServer('VnXGlobalSystemsClient:GetDiscordID', () => {
 });
 
 alt.onServer("VnXGlobalSystemsClient:SetPedCanRagdoll", (bool) => {
-    try { game.setPedCanRagdoll(localplayer.scriptID, bool); }
+    try { game.setPedCanRagdoll(localPlayer.scriptID, bool); }
     catch{ }
 });
 alt.onServer("VnXGlobalSystemsClient:SetProofs", (bool) => {
-    try { game.setEntityProofs(localplayer.scriptID, true, false, false, false, false, false, false, false); }
+    try { game.setEntityProofs(localPlayer.scriptID, true, false, false, false, false, false, false, false); }
     catch{ }
 });
 
 
-alt.onServer('VnXGlobalSystemsClient:Kick', (reason) => {
+alt.onServer("VnXGlobalSystemsClient:Kick", (reason) => {
     if (kicked) return;
     alt.logWarning("------------------------------------");
     alt.logError("-------- Kicked by Anticheat -------");
@@ -42,12 +42,12 @@ alt.onServer('VnXGlobalSystemsClient:Kick', (reason) => {
     alt.logWarning("------------------------------------");
     kicked = true;
 });
-alt.onServer('VnXGlobalSystemsClient:KickGlobal', () => {
+alt.onServer("VnXGlobalSystemsClient:KickGlobal", () => {
     if (kicked) return;
     alt.log("~b~~c~------------------------------------");
     alt.log("~r~--------        You´re Globally Banned by VenoX Global Systems!             -------");
     alt.log("~r~--------        E-Mail https://forum.altv.mp/profile/1466-solid_snake/      -------");
     alt.log("~b~~c~------------------------------------");
-    alt.emitServer('VnXGlobalSystems:KickPlayer');
+    alt.emitServer("VnXGlobalSystems:KickPlayer");
     kicked = true;
 });
