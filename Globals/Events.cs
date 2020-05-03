eb69ec01c7956e431b5288d9ab76c1acc10514bf
Solid <kahmadvand@outlook.com>
2020-05-03 10:32:04 +0200

    Fixed WeaponDamage call

--- Globals/Events.cs
+++ Globals/Events.cs
@@ -47,7 +47,7 @@ namespace VnXGlobalSystems.Globals
         [ScriptEvent(ScriptEventType.WeaponDamage)]
         public static void WeaponDamage(PlayerModel player, PlayerModel target, uint weapon, ushort dmg, Position offset, BodyPart bodypart)
         {
-            try { WeaponSync.WeaponDamage(player, target, weapon, dmg, offset, bodypart); Alt.Emit("GlobalSystems:OnEntityHit", player, target); }
+            try { if (!Functions.AnticheatModel.AntiGodmode) { return; } WeaponSync.WeaponDamage(player, target, weapon, dmg, offset, bodypart); Alt.Emit("GlobalSystems:OnEntityHit", player, target); }
             catch { }
         }
 
@@ -59,7 +59,7 @@ namespace VnXGlobalSystems.Globals
 
         ////////////////////////// Weapon Anticheat /////////////////////////////////////////////////////////
         [ServerEvent("GlobalSystems:GiveWeapon")]
-        public static void OnWeaponEventCall(PlayerModel player, uint WeaponHash, byte ammo, bool selectWeapon) => player.GivePlayerWeapon(WeaponHash, ammo, selectWeapon);
+        public static void OnWeaponEventCall(PlayerModel player, uint WeaponHash, int ammo, bool selectWeapon) => player.GivePlayerWeapon(WeaponHash, ammo, selectWeapon);
 
         [ServerEvent("GlobalSystems:RemovePlayerWeapon")]
         public static void RemovePlayerWeapon(PlayerModel player, uint WeaponHash) => player.RemovePlayerWeapon(WeaponHash);
@@ -94,32 +94,34 @@ namespace VnXGlobalSystems.Globals
         {
             try
             {
+                if (player.DiscordID.Length > 0) { return; }
                 player.DiscordID = DiscordID;
-                //Core.Debug.OutputDebugString("Discord ID [" + player.Name + "] : " + DiscordID);
+                Core.Debug.OutputDebugString("Discord ID [" + player.Name + "] : " + DiscordID);
+                Functions.CheckPlayerGlobalBans(player);
             }
             catch (Exception ex) { Core.Debug.CatchExceptions("Global-Systems:UpdateDiscordInfo", ex); }
         }
-        public static void GivePlayerWeapon(this PlayerModel player, uint WeaponHash, byte ammo, bool selectWeapon)
+        public static void GivePlayerWeapon(this PlayerModel player, uint WeaponHash, int ammo, bool selectWeapon)
         {
-            try { player.GiveWeapon(WeaponHash, ammo, selectWeapon); player.Weapons.Add(WeaponHash); }
+            try { if (player == null) { return; } player.Weapons.Add(WeaponHash); player.GiveWeapon(WeaponHash, ammo, selectWeapon); }
             catch (Exception ex) { Core.Debug.CatchExceptions("Global-Systems:GivePlayerWeapon", ex); }
         }
 
         public static void RemovePlayerWeapon(this PlayerModel player, uint WeaponHash)
         {
-            try { player.RemoveWeapon(WeaponHash); player.Weapons.Remove(WeaponHash); }
+            try { if (player == null) { return; } player.RemoveWeapon(WeaponHash); player.Weapons.Remove(WeaponHash); }
             catch (Exception ex) { Core.Debug.CatchExceptions("Global-Systems:RemovePlayerWeapon", ex); }
         }
 
         public static void RemoveAllPlayerWeapon(this PlayerModel player)
         {
-            try { player.RemoveAllWeapons(); player.Weapons.Clear(); }
+            try { if (player == null) { return; } player.RemoveAllWeapons(); player.Weapons.Clear(); }
             catch (Exception ex) { Core.Debug.CatchExceptions("Global-Systems:RemoveAllPlayerWeapon", ex); }
         }
 
         public static void Position(this PlayerModel player, Vector3 position)
         {
-            try { player.LastPosition = position; player.Position = position; }
+            try { if (player == null) { return; } player.LastPosition = position; player.Position = position; Core.Debug.OutputDebugString("Called from " + player.Name + " : " + position); }
             catch (Exception ex) { Core.Debug.CatchExceptions("Global-Systems:Position", ex); }
         }
     }
