32fbcf2c64d281deb31d637ceaa20bf07e6f3d67
Solid <kahmadvand@outlook.com>
2020-05-09 23:19:55 +0200

    [Models] : Added NetworkModelClass

---
+++ Models/NetworkModel.cs
@@ -0,0 +1,13 @@
+﻿namespace VnXGlobalSystems.Models
+{
+    public class Networking
+    {
+        public string IP { get; set; }
+        public string CountryCode { get; set; }
+        public string CountryName { get; set; }
+        public string Asn { get; set; }
+        public string Isp { get; set; }
+        public string Block { get; set; }
+        public string Hostname { get; set; }
+    }
+}
