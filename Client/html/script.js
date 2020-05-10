13496545ec1b604a648bf5f3b473d5cd5c191e42
Solid <kahmadvand@outlook.com>
2020-05-10 23:15:15 +0200

    [Client] : Added PrivacyPolicy Window & Event-Handlers.

---
+++ Client/html/script.js
@@ -0,0 +1,3 @@
+if ("alt" in window) {
+
+}
--- Client/script.js
+++ Client/script.js
@@ -10,6 +10,7 @@ import * as game from "natives";
 
 let localplayer = alt.Player.local;
 let kicked = false;
+let TermsBrowser;
 
 alt.log("VnXGlobalSystemsClient:Loaded");
 
@@ -22,29 +23,55 @@ alt.onServer('VnXGlobalSystemsClient:GetDiscordID', () => {
 });
 
 alt.onServer("VnXGlobalSystemsClient:SetPedCanRagdoll", (bool) => {
-    try { game.setPedCanRagdoll(localplayer.scriptID, bool); }
-    catch{ }
+	try { game.setPedCanRagdoll(localplayer.scriptID, bool); }
+	catch{ }
 });
 alt.onServer("VnXGlobalSystemsClient:SetProofs", (bool) => {
-    try { game.setEntityProofs(localplayer.scriptID, true, false, false, false, false, false, false, false); }
-    catch{ }
+	try { game.setEntityProofs(localplayer.scriptID, true, false, false, false, false, false, false, false); }
+	catch{ }
 });
 
 
 alt.onServer('VnXGlobalSystemsClient:Kick', (reason) => {
-    if (kicked) return;
-    alt.log("~c~------------------------------------");
-    alt.log("~r~-------- Kicked by Anticheat -------");
-    alt.log("~c~------------------------------------");
-	alt.emitServer('VnXGlobalSystems:KickPlayer');
-    kicked = true;
+	try {
+		if (kicked) return;
+		alt.log("~b~~c~------------------------------------");
+		alt.log("~r~--------        Kicked by VenoX Global Systems!        -------");
+		alt.log("~b~~c~------------------------------------");
+		alt.emitServer('VnXGlobalSystems:KickPlayer');
+		kicked = true;
+	}
+	catch{ }
 });
 alt.onServer('VnXGlobalSystemsClient:KickGlobal', () => {
-    if (kicked) return;
-    alt.log("~c~------------------------------------");
-    alt.log("~r~--------        You´re Globally Banned by VenoX Global Systems!             -------");
-    alt.log("~r~--------        E-Mail https://forum.altv.mp/profile/1466-solid_snake/      -------");
-    alt.log("~b~~c~------------------------------------");
-    alt.emitServer('VnXGlobalSystems:KickPlayer');
-    kicked = true;
+	try {
+		if (kicked) return;
+		alt.log("~b~~c~------------------------------------");
+		alt.log("~r~--------        You´re Globally Banned by VenoX Global Systems!             -------");
+		alt.log("~r~--------        E-Mail https://forum.altv.mp/profile/1466-solid_snake/      -------");
+		alt.log("~b~~c~------------------------------------");
+		alt.emitServer('VnXGlobalSystems:KickPlayer');
+		kicked = true;
+	}
+	catch{ }
+});
+
+alt.onServer('VnXGlobalSystemsClient:ShowPrivacyPolicy', () => {
+	if (TermsBrowser) { return; }
+	TermsBrowser = new alt.WebView("http://resource/Client/html/main.html");
+	alt.showCursor(true);
+	alt.toggleGameControls(false);
+	TermsBrowser.focus();
+	TermsBrowser.on('VnXGlobalSystemsClient:ShowPrivacyPolicy:Accept', () => { alt.emitServer('VnXGlobalSystems:PrivacyPolicy', 1); });
+	TermsBrowser.on('VnXGlobalSystemsClient:ShowPrivacyPolicy:Deny', () => { alt.emitServer('VnXGlobalSystems:PrivacyPolicy', 0); });
+});
+
+alt.onServer('VnXGlobalSystemsClient:HidePrivacyPolicy', () => {
+	if (TermsBrowser) {
+		TermsBrowser.destroy();
+		TermsBrowser = null;
+		alt.showCursor(false);
+		alt.toggleGameControls(true);
+		return;
+	}
 });
