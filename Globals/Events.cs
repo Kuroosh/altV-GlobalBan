6e48645d2895d5055e1a48fccd0aa7681fb02ee2
Solid <kahmadvand@outlook.com>
2020-05-10 23:08:22 +0200

    [Events] : Added Support for Privacy Data.

--- Globals/Events.cs
+++ Globals/Events.cs
@@ -1,6 +1,9 @@
 ﻿using AltV.Net;
 using AltV.Net.Data;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Numerics;
 using System.Threading;
 using VnXGlobalSystems.Models;
@@ -15,11 +18,40 @@ namespace VnXGlobalSystems.Globals
             Functions.timer = new Timer(Functions.OnUpdate, null, Constants.UPDATEINTERVAL, Constants.UPDATEINTERVAL);
             Functions.LoadMainFunctions();
             Database.Main.OnResourceStart();
-            Core.Debug.WriteLogs("debug", "Server Started with Debug Mode true!");
+            LoadPrivacyAccounts();
+            if (Constants.AWESOME_SNAKE_MODE)
+            {
+                Core.Debug.WriteLogs("debug", "Server Started with Debug Mode true!");
+            }
         }
         public static void OnResourceStop()
         {
             Functions.UnloadMainFunctions();
+        }
+
+        public static void LoadPrivacyAccounts()
+        {
+            List<PrivacyModel> privacyClasses = JsonConvert.DeserializeObject<List<PrivacyModel>>(File.ReadAllText(Alt.Server.Resource.Path + "/settings/privacy.json"));
+            if (privacyClasses.Count <= 0) { return; }
+            foreach (PrivacyModel privacyUser in privacyClasses)
+            {
+                Constants.PrivacyAcceptedPlayers.Add(privacyUser);
+            }
+        }
+        public static bool PlayerAcceptedPrivacyPolicy(PlayerModel player)
+        {
+            try
+            {
+                foreach (PrivacyModel privacyClass in Constants.PrivacyAcceptedPlayers)
+                {
+                    if (privacyClass.HardwareId == player.HardwareIdHash.ToString() || privacyClass.HardwareIdExHash == player.HardwareIdExHash.ToString() || privacyClass.IP == player.Ip.ToString() || privacyClass.SocialID == player.SocialClubId.ToString())
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch { return false; }
         }
 
         ////////////////////////// Player Events /////////////////////////////////////////////////////////
@@ -28,9 +60,16 @@ namespace VnXGlobalSystems.Globals
         {
             try
             {
+                Core.Main.CheckIP(player);
                 player.Emit("VnXGlobalSystemsClient:GetDiscordID");
                 Functions.CheckPlayerGlobalBans(player);
                 Main.ConnectedPlayers.Add(player);
+                if (!PlayerAcceptedPrivacyPolicy(player))
+                {
+                    player.Emit("VnXGlobalSystemsClient:ShowPrivacyPolicy");
+                    return;
+                }
+                Alt.Emit("GlobalSystems:PlayerReady", player);
             }
             catch (Exception ex) { Core.Debug.CatchExceptions("OnPlayerConnect", ex); }
         }
@@ -48,7 +87,7 @@ namespace VnXGlobalSystems.Globals
         [ScriptEvent(ScriptEventType.WeaponDamage)]
         public static void WeaponDamage(PlayerModel player, PlayerModel target, uint weapon, ushort dmg, Position offset, BodyPart bodypart)
         {
-            try { if (!Functions.AnticheatModel.AntiGodmode) { return; } WeaponSync.WeaponDamage(player, target, weapon, dmg, offset, bodypart); Alt.Emit("GlobalSystems:OnEntityHit", player, target); }
+            try { if (target == null) { return; } if (!Functions.AnticheatModel.AntiGodmode) { return; } WeaponSync.WeaponDamage(player, target, weapon, dmg, offset, bodypart); Alt.Emit("GlobalSystems:OnEntityHit", player, target); }
             catch { }
         }
 
@@ -57,6 +96,9 @@ namespace VnXGlobalSystems.Globals
 
         [ClientEvent("VnXGlobalSystems:KickPlayer")]
         public static void KickPlayer(PlayerModel player) => player?.Kick(reason: String.Empty);
+
+        [ClientEvent("VnXGlobalSystems:PrivacyPolicy")]
+        public static void SetPrivacyPolicy(PlayerModel player, int privacy) => player?.SetPrivacyPolicy(privacy);
 
         ////////////////////////// Weapon Anticheat /////////////////////////////////////////////////////////
         [ServerEvent("GlobalSystems:GiveWeapon")]
@@ -72,9 +114,49 @@ namespace VnXGlobalSystems.Globals
         [ServerEvent("GlobalSystems:PlayerPosition")]
         public static void PlayerPosition(PlayerModel player, Vector3 position) => player.Position(position);
 
+        [ServerEvent("GlobalSystems:PlayerTeam")]
+        public static void Playerteam(PlayerModel player, int TeamId) => player.SetPlayerTeam(TeamId);
+
+
     }
     public static class EventFunctions
     {
+        public static void SetPlayerTeam(this PlayerModel player, int TeamId)
+        {
+            try
+            {
+                player.Team = TeamId;
+            }
+            catch (Exception ex) { Core.Debug.CatchExceptions("Global-Systems:SetPlayerTeam", ex); }
+        }
+        public static void SetPrivacyPolicy(this PlayerModel player, int privacy)
+        {
+            try
+            {
+                switch (privacy)
+                {
+                    case 0:
+                        player.KickPlayer(String.Empty);
+                        return;
+                    case 1:
+                        PrivacyModel privacyClass = new PrivacyModel
+                        {
+                            Name = player.Name,
+                            HardwareId = player.HardwareIdHash.ToString(),
+                            HardwareIdExHash = player.HardwareIdExHash.ToString(),
+                            IP = player.Ip.ToString(),
+                            SocialID = player.SocialClubId.ToString()
+                        };
+                        string Json = JsonConvert.SerializeObject(privacyClass);
+                        Core.Debug.WriteJsonString("privacy", Json);
+                        Constants.PrivacyAcceptedPlayers.Add(privacyClass);
+                        player.Emit("VnXGlobalSystemsClient:HidePrivacyPolicy");
+                        Alt.Emit("GlobalSystems:PlayerReady", player);
+                        return;
+                }
+            }
+            catch (Exception ex) { Core.Debug.CatchExceptions("Global-Systems:SetPrivacyPolicy", ex); }
+        }
         public static void KickPlayer(this PlayerModel player, string reason)
         {
             try
