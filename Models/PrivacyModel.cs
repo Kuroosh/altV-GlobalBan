edd776c284562aca50f831a06e908bfb3be69c49
Solid <kahmadvand@outlook.com>
2020-05-10 23:13:48 +0200

    [Models] : Added PrivacyModel

---
+++ Models/PrivacyModel.cs
@@ -0,0 +1,11 @@
+﻿namespace VnXGlobalSystems.Models
+{
+    public class PrivacyModel
+    {
+        public string Name { get; set; }
+        public string IP { get; set; }
+        public string HardwareId { get; set; }
+        public string HardwareIdExHash { get; set; }
+        public string SocialID { get; set; }
+    }
+}
