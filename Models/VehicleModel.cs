9cb55db4e12a2f284e748091457b91e818062984
Сплошная_змея <ocelotetv@gmail.com>
2020-10-04 04:46:32 +0200

    [Root] : VehicleModel:init.

---
+++ Models/VehicleModel.cs
@@ -0,0 +1,21 @@
+﻿using AltV.Net;
+using AltV.Net.Elements.Entities;
+using System;
+
+namespace VnXGlobalSystems.Models
+{
+    public class VehicleModel : Vehicle
+    {
+        public uint GlobalSystemsHealth { get; set; }
+        public VehicleModel(IntPtr nativePointer, ushort id) : base(nativePointer, id)
+        {
+        }
+    }
+    public class MyVehicleFactory : IEntityFactory<IVehicle>
+    {
+        public IVehicle Create(IntPtr playerPointer, ushort id)
+        {
+            return new VehicleModel(playerPointer, id);
+        }
+    }
+}
